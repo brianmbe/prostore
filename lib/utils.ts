4293923badb3c13172396f6fd8ca9d029c384db3
brianmbe <briannmbe@gmail.com>
2025-01-05 18:51:00 +0300

    add validators for schema

--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,10 @@ export function cn(...inputs: ClassValue[]) {
 export function convertPrismaObjToObj<T>(value: T) {
   return JSON.parse(JSON.stringify(value));
 }
+
+// Format numbers with decimal places and commas
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimals] = num.toString().split(".");
+
+  return decimals ? `${int}.${decimals.padEnd(2, '0')}` : `${int}.00`; 
+}
---
+++ lib/validators.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { formatNumberWithDecimal } from "./utils";
+
+const currency = z.number().refine((val) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(+val)), 'Invalid price format')
+
+// Schema for inserting products
+export const insertProductSchema = z.object({
+  name: z.string().min(3, "Name must be atleast 3 characters"),
+  slug: z.string().min(3, "Slug must be atleast 3 characters"),
+  category: z.string().min(3, "Category must be atleast 3 characters"),
+  brand: z.string().min(3, "Brand must be atleast 3 characters"),
+  description: z.string().min(3, "Category must be atleast 3 characters"),
+  stock: z.coerce.number(),
+  images: z.array(z.string()).min(1, "Atleast one image is required"),
+  rating: z.number().positive().int(),
+  isFeatured: z.boolean(),
+  banner: z.string().nullable(),
+    price: currency,
+});
