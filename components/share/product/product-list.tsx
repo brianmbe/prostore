59b2313b78ab63eaa1916954a0c398f51109ffdd
brianmbe <briannmbe@gmail.com>
2025-01-06 12:55:19 +0300

    add Prisma & neon configuration

--- components/share/product/product-list.tsx
+++ components/share/product/product-list.tsx
@@ -1,3 +1,4 @@
+import { Product } from "@/types";
 import ProductCard from "./product-card";
 
 export default function ProductList({
@@ -5,7 +6,7 @@ export default function ProductList({
   title,
   limit,
 }: {
-  data: any;
+  data: Product[];
   title?: string;
   limit?: number;
 }) {
@@ -17,7 +18,7 @@ export default function ProductList({
 
       {data.length > 0 ? (
         <div className="gap-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-          {limitedData.map((product: any) => (
+          {limitedData.map((product: Product) => (
             <ProductCard key={product.name} product={product} />
           ))}
         </div>
--- lib/validators.ts
+++ lib/validators.ts
@@ -2,8 +2,11 @@ import { z } from "zod";
 import { formatNumberWithDecimal } from "./utils";
 
 const currency = z
-  .number()
-  .refine((val) => /^\d+(\.\d{2})?$/.test(val), "Invalid price format");
+  .string()
+  .refine(
+    (val) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(val)),
+    "Invalid price format"
+  );
 
 // Schema for inserting products
 export const insertProductSchema = z.object({
@@ -12,7 +15,7 @@ export const insertProductSchema = z.object({
   category: z.string().min(3, "Category must be atleast 3 characters"),
   brand: z.string().min(3, "Brand must be atleast 3 characters"),
   description: z.string().min(3, "Category must be atleast 3 characters"),
-  stock: z.coerce.number(),
+  stock: z.coerce.number(), // comes as string from form and changes to number
   images: z.array(z.string()).min(1, "Atleast one image is required"),
   rating: z.number().positive().int(),
   isFeatured: z.boolean(),
